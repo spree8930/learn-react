390724c9c85979bb5f4b74d0a482201f721275bd
spree8930 <118305999+spree8930@users.noreply.github.com>
2023-01-20 13:17:32 -0800

    Add badge text

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Card(props) {
+    let badgeText;
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if(props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <img className="card-image" src={`../images/${props.coverImg}`} />
             <div className="card-stats">
                 <FontAwesomeIcon className="card-star" icon={faStar} />
-                <span> {props.rating} </span>
-                <span className="gray"> ({props.reviewCount}) . </span>
+                <span> {props.stats.rating} </span>
+                <span className="gray"> ({props.stats.reviewCount}) . </span>
                 <span className="gray"> {props.location} </span>
             </div>
             <p className="card-title">{props.title}</p>
