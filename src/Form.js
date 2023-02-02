a36ba7de6b148a14e79361e9286fc0f59ad8660a
spree8930 <118305999+spree8930@users.noreply.github.com>
2023-02-02 11:22:59 -0800

    Added email

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Form() {
     const [formData, setFormData] = React.useState({
         firstName: "",
-        lastName: ""
+        lastName: "",
+        email: ""
     });
 
     function handleChange(event){
@@ -17,7 +18,8 @@ export default function Form() {
         <form>
             <input type="text" name="firstName" placeholder="First Name" onChange={handleChange}></input>
             <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange}></input>
-            <p>{formData.firstName} {formData.lastName}</p>
+            <input type="email" name="email" placeholder="Email" onChange={handleChange}></input>
+            <p>{formData.firstName} {formData.lastName} {formData.email}</p>
         </form>
     );
 }
